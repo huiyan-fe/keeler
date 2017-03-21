2fe5babd2e78a6fc88ceb808c415330a61454eb9
Mofei Zhu <13761509829@163.com>
2017-03-21 13:31:58 +0800

    update gulpfile.js

--- bin/keel.js
+++ bin/keel.js
@@ -6,15 +6,12 @@ const program = require('commander');
 const init = require('../lib/init');
 const build = require('../lib/build');
 
-
 program
     .version(require('../package.json').version)
     .usage('[options] [project name]')
     .option('-i, --init', 'init project')
     .option('-b, --build [type]', 'build project')
     .parse(process.argv);
-
-console.log('program', program);
 
 if (program.init) {
     const pwd = process.env.PWD;
@@ -28,4 +25,9 @@ if (program.build) {
     } else {
         build.dev(process, pwd);
     }
+};
+
+
+if (program.version) {
+    console.log(require('../package.json').version)
 };
