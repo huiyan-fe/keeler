c769802538064a374db83712551adbb27a37a2d5
Mofei Zhu <13761509829@163.com>
2017-09-15 17:04:17 +0800

    Update webpack.config.js

--- demo/react-sass-es6/webpack.config.js
+++ demo/react-sass-es6/webpack.config.js
@@ -1,24 +1,42 @@
 const package = require("./package.json");
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 process.traceDeprecation = true;
 
 module.exports = {
     entry: {
-        'app/index': './app/index.jsx',
+        'app/index': './src/app/index.jsx',
         'app/vendor': ['react', 'react-dom']
     },
     output: {
         filename: `[name].${package.version}.js`,
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/page/index.html',
+            hash: true
+        })
+    ],
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
     },
     module: {
         loaders: [{
-            test: /\.jsx$/,
-            loader: 'babel-loader'
-        }]
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+            }
+        ]
     }
 }
