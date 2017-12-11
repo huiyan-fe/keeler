ab9059db484d284c5e693096291c847ce0e292b1
zmofei <13761509829@163.com>
2017-12-11 16:54:39 +0800

    update

--- bin/keeler.js
+++ bin/keeler.js
@@ -5,13 +5,32 @@
 const program = require('commander');
 const init = require('../lib/init');
 const build = require('../lib/build');
+const add = require('../lib/add').add;
 
 program
     .version(require('../package.json').version)
-    .usage('[options] [project name]')
+    .usage('[options] [param]')
     .option('-i, --init', 'init project')
-    .option('-b, --build [type]', 'build project')
-    .parse(process.argv);
+    .option('-b, --build', 'build project')
+
+
+program
+    .command('init')
+    .description('init projiect')
+    .action((options) => {
+        const pwd = process.env.PWD;
+        init.init(process, pwd);
+    })
+
+program
+    .command('add [name]')
+    .description('add an new page \t 添加一个新的页面')
+    .action((name) => {
+        const pwd = process.env.PWD;
+        add(process, pwd, name);
+    })
+
+program.parse(process.argv);
 
 if (program.init) {
     const pwd = process.env.PWD;
